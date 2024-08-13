f1ec165c944282ecd66565b51e2c8ed39456e3d5
asare-grant <168415236+asare-grant@users.noreply.github.com>
2024-08-13 11:25:45 +0100

    Update ChatBox.jsx

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -148,7 +148,7 @@ const ChatBox = () => {
         </div>
     </div>
   )
-  : <div className={`chat-welcom ${chatVisible? "":"hidden"}`}>
+  : <div className={`chat-welcome ${chatVisible? "":"hidden"}`}>
         <img src={assets.logo_icon} alt="" />
         <p>Chat anytime, anywhere</p>
     </div>
